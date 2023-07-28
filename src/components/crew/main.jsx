a0425a7c5be5c2c396bf96a2e418951a92bea26d
Ana María <developeranamaria@gmail.com>
2023-07-28 08:42:55 -0500

    Try

--- src/components/crew/main.jsx
+++ src/components/crew/main.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import dataObject from '../../services/data.js';
 import "./main.scss"
 
 function Crew() {
+  const [activeMemberIndex, setActiveMemberIndex] = useState(0);
+  const totalMembers = dataObject.crew.length;
+  const activeMember = dataObject.crew[activeMemberIndex];
+
+  const handleIndicatorClick = (index) => {
+    setActiveMemberIndex(index);
+  };
+
   return (
     <>
-    <div>SOY EL CREW</div>
     <div className='background_crew'></div>
+    <section><h1>002</h1> <h1>MEET YOUR CREW</h1></section>
+      <div>
+        <p>{activeMember.role}</p>
+        <p>{activeMember.name}</p>
+        <p>{activeMember.bio}</p>
+        <div className="indicator-container">
+          {dataObject.crew.map((_, index) => (
+          <span
+            key={index}
+            className={index === activeMemberIndex ? 'indicator active' : 'indicator'}
+            onClick={() => handleIndicatorClick(index)}
+          ></span>
+          ))}
+        </div>
+      </div>
+      <img src={activeMember.images.png} alt={activeMember.name} />
     </>
-  )
+  );
 }
 
-export default Crew
+export default Crew;
